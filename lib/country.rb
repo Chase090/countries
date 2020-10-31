e45a40415032f49fb14789803500620526290b06
Chase090 <carloyam21@icloud.com>
2020-10-31 03:00:41 -0700

    updated readme

--- lib/country.rb
+++ lib/country.rb
@@ -12,13 +12,7 @@ class Country
         @@all
     end
 
-    # def self.find_by_city(capital_city)
-    #     self.all.find{|capital| capital.capital_city == capital_city || capital.capital_city.downcase == capital_city}
-    # end
-    # def list_of_high_income_countries
-    #     self.collect do ||
-        
-    # end
+  
      
 
 end
